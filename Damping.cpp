f0e765c70b804b3e2aa1d3d28c9f80cf88864967
Aingaran <eaingaran@gmail.com>
2016-04-25 16:39:40 +0530

    Uploaded Damping.cpp
    This is a script that calculates damping

---
+++ Damping.cpp
@@ -0,0 +1,20 @@
+// Damping.cpp : Aingaran Elango
+// To calculate damping ratio from amplitude of oscillation
+
+#include<iostream>
+#include<conio.h>
+#include <math.h> 
+
+using namespace std;
+
+int main()
+{
+	double A1,A2,damping,lA;
+	cout<<"Enter the amplitudes A1 and A2"<<endl;
+	cin>>A1>>A2;
+	lA=log(A1/A2);
+	damping=(lA/(sqrt(((6.28)*(6.28))+(lA*lA))));
+	cout<<"damping ratio is "<<damping<<endl;
+	//getch();
+	return(0);
+} 
